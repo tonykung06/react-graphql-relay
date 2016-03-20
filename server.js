b26790d10c923ddb90022aefc5b8296a04c7d83f
tonykung <tonykung06@gmail.com>
2016-03-20 22:11:25 +0800

    benefits in using graphql

--- server.js
+++ server.js
@@ -31,6 +31,22 @@ MongoClient.connect(config.MONGO_CONNECTION, (err, database) => {
 	//
 	//introspection on the capability provided by the graphql server, i.e. the graphql structure:
 	//{__type(name: "ReadAnything"){fields{name}}}
+	//
+	//1. field argument
+	//2. aliases
+	//3. fragments
+	//
+	//benefits:
+	//1. easy-to-write queries if you know the data that you need
+	//2. the queries describes the shape of the response
+	//3. no over-fetching and under-fetching
+	//4. can customize the data that you get with aliases and field arguments
+	//5. can query objects and their connections, and nest as deep as you need, and get all that data in a single round trip
+	//6. as views change their data requirement, we don't need to change the graphql server as long as it is using the capabilities published by the server
+	//7. built-in accurate and always up-to-date documentation
+	//8. queries are composable and re-usable by taking advantages of fragments
+	//9. can re-use existing logic in the server code
+	//10. mutations are just queries, same structure and same benefits
 	app.use('/graphql', GraphQLHTTP({
 		schema: schema(db),
 		graphiql: true //providing type system UI at /graphql
